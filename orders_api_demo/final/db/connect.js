ab6316d86fddde336b2a0474d03ecdae7ac18e25
Saad Shamim <mohammadsaad0606@gmail.com>
2024-03-13 15:47:22 +0530

    Updated database config

--- orders_api_demo/final/db/connect.js
+++ orders_api_demo/final/db/connect.js
@@ -5,11 +5,11 @@ const mysql2 = require('mysql2');
 
 // Create a connection pool
 const pool = mysql2.createPool({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: '0987poiu',
-  database: 'test'
+  host: '172.26.0.15',
+  // port: '3306',
+  user: 'staging_user',
+  password: 'Db@user2024',
+  database: 'developer_db'
 });
 
 module.exports = pool;
