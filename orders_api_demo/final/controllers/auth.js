a9d5da843e1e5d505cd4c0737adae8d005a41e69
Saad Shamim <mohammadsaad0606@gmail.com>
2024-05-15 12:30:24 +0530

    Implemented otp based login

--- orders_api_demo/final/controllers/auth.js
+++ orders_api_demo/final/controllers/auth.js
@@ -8,6 +8,9 @@ const pool = require('../db/connect');
 // const postmark = require('postmark');
 // const nodemailer = require('nodemailer');
 // const postmarkTransport = require('nodemailer-postmark-transport');
+const randomstring = require('randomstring');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 
 // Function to hash password
@@ -36,7 +39,7 @@ const generateJWT = (userId, name, role) => {
 exports.register = async (req, res) => {
     try {
         // Extract user details from request body
-        const { name, email, password, role } = req.body;
+        const { id, name, email, password, role } = req.body;
 
         // Check if the user already exists
         const checkUserQuery = `SELECT * FROM users WHERE email = ?`;
@@ -58,13 +61,13 @@ exports.register = async (req, res) => {
         const hashedPassword = await hashPassword(password);
 
         // Insert user into the database
-        const insertUserQuery = `INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)`;
+        const insertUserQuery = `INSERT INTO users (id, name, email, password, role) VALUES (?, ?, ?, ?, ?)`;
 
         // Get a new connection from the pool
         const newConnection = await pool.getConnection();
 
         // Execute the query
-        const [result] = await newConnection.query(insertUserQuery, [name, email, hashedPassword, role]);
+        const [result] = await newConnection.query(insertUserQuery, [id, name, email, hashedPassword, role]);
 
         // Release the connection back to the pool
         newConnection.release();
@@ -90,16 +93,16 @@ exports.login = async (req, res) => {
 
         // Find the user by email
         const getUserQuery = `SELECT * FROM users WHERE email = ?`;
-        
+
         // Get a connection from the pool
         const connection = await pool.getConnection();
-        
+
         // Execute the query
         const [results] = await connection.query(getUserQuery, [email]);
-        
+
         // Release the connection back to the pool
         connection.release();
-        
+
         if (results.length === 0) {
             return res.status(404).json({ message: 'User not found' });
         }
@@ -116,7 +119,7 @@ exports.login = async (req, res) => {
         const token = generateJWT(user.id, user.name, user.role);
 
         // Send response with token
-        res.status(200).json({ success: true, user: {id: user.id, name: user.name, role: user.role }, token });
+        res.status(200).json({ success: true, user: { id: user.id, name: user.name, role: user.role }, token });
     } catch (error) {
         console.error('Error logging in user:', error);
         res.status(500).json({ message: error.message });
@@ -127,11 +130,153 @@ exports.login = async (req, res) => {
 // Controller for user logout 
 exports.logout = async (req, res) => {
     try {
-        
+
         res.status(200).json({ message: 'User logged out successfully' });
     } catch (error) {
         console.error('Error logging out user:', error);
         res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+// Function to send OTP to user's email
+// exports.sendOTP = async (req, res) => {
+//     const { email } = req.body;
+    
+//     // Generate OTP
+//     const otp = randomstring.generate({
+//         length: 6,
+//         charset: 'numeric'
+//     });
+
+//     // Send OTP to user's email
+//     try {
+//         // Store OTP in the database
+//         const insertQuery = 'INSERT INTO otps (email, otp) VALUES (?, ?)';
+//         await pool.query(insertQuery, [email, otp]);
+
+        // const transporter = nodemailer.createTransport({
+        //     // Configure your email provider here
+        //     service: 'gmail',
+        //     auth: {
+        //         user: 'mohammadsaad0606@gmail.com', // Your email address
+        //         pass: 'zuij xxpl rrod ttif' // Your email password or app-specific password
+        //     }
+        // });
+
+        // const mailOptions = {
+        //     from: 'mohammadsaad0606@gmail.com',
+        //     to: email,
+        //     subject: 'OTP for Login',
+        //     text: `Your OTP for login is: ${otp}`
+        // };
+
+//         await transporter.sendMail(mailOptions);
+
+//         res.status(200).json({ success: true, message: 'OTP sent to your email' });
+//     } catch (error) {
+//         console.error('Error sending OTP:', error);
+//         res.status(500).json({ success: false, message: 'Failed to send OTP' });
+//     }
+// };
+
+
+// Function to send OTP to user's email
+exports.sendOTP = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+
+        const selectUserQuery = 'SELECT * FROM users WHERE email = ?';
+        const [userRows] = await pool.query(selectUserQuery, [email]);
+
+        if (userRows.length === 0) {
+            // Email is not registered
+            return res.status(400).json({ success: false, message: 'Email is not registered' });
+        }
+
+        // Check if a record already exists for the given email address
+        const selectQuery = 'SELECT * FROM otps WHERE email = ?';
+        const [rows] = await pool.query(selectQuery, [email]);
+
+        let otp;
+        // let otpTimestamp;
+        if (rows.length > 0) {
+            // Update the existing record with a new OTP
+            otp = randomstring.generate({
+                length: 6,
+                charset: 'numeric'
+            });
+            // otpTimestamp =  new Date(rows[0].timestamp).getTime();
+            const updateQuery = 'UPDATE otps SET otp = ?, timestamp = NOW() WHERE email = ?';
+            await pool.query(updateQuery, [otp, email]);
+        } else {
+            // Generate a new OTP and insert a new record
+            otp = randomstring.generate({
+                length: 6,
+                charset: 'numeric'
+            });
+            const insertQuery = 'INSERT INTO otps (email, otp, timestamp) VALUES (?, ?, ?)';
+            await pool.query(insertQuery, [email, otp, timestamp]);
+        }
+
+        // Send OTP to user's email
+        const transporter = nodemailer.createTransport({
+            // Configure your email provider here
+            service: 'gmail',
+            auth: {
+                user: 'mohammadsaad0606@gmail.com', // Your email address
+                pass: 'zuij xxpl rrod ttif' // Your email password or app-specific password
+            }
+        });
+
+        const mailOptions = {
+            from: 'mohammadsaad0606@gmail.com',
+            to: email,
+            subject: 'OTP for Login',
+            text: `Your OTP for login is: ${otp}`
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        res.status(200).json({ success: true, message: 'OTP sent to your email' });
+    } catch (error) {
+        console.error('Error sending OTP:', error);
+        res.status(500).json({ success: false, message: 'Failed to send OTP' });
+    }
+};
+
+// Function to verify OTP
+exports.verifyOTP = async (req, res) => {
+    const { email, otp } = req.body;
+
+    try {
+        // Retrieve the stored OTP for the user's email from the database
+        const selectQuery = 'SELECT * FROM otps WHERE email = ? ';
+
+        // const query = 'SELECT otp, timestamp FROM otps WHERE email = ? ORDER BY timestamp DESC LIMIT 1';
+        const [rows] = await pool.query(selectQuery, [email]);
+
+        if (rows.length === 0) {
+            // No OTP found for the user's email
+            return res.status(400).json({ success: false, message: 'No OTP found for this email' });
+        }
+
+        const storedOTP = rows[0].otp;
+        const timestamp = new Date(rows[0].timestamp).getTime();
+        const currentTime = new Date().getTime();
+        const timeElapsed = currentTime - timestamp;
+
+        if (otp == storedOTP && timeElapsed <= 10 * 60 * 1000) {
+            // OTP is valid
+            res.status(200).json({ success: true, message: 'OTP verification successful' });
+        } else {
+            // OTP is invalid
+            res.status(400).json({ success: false, message: 'Invalid OTP' });
+        }
+        
+    } catch (error) {
+        console.error('Error verifying OTP:', error);
+        res.status(500).json({ success: false, message: 'Failed to verify OTP' });
     }
 };
 
@@ -140,19 +285,19 @@ exports.logout = async (req, res) => {
 
 // exports.sendOTP = (req, res) => {
 //     const { email } = req.body;
-  
+
 //     // Generate OTP
 //     const OTP = otpGenerator.generate(6, { digits: true, alphabets: false, upperCase: false, specialChars: false });
-  
+
 //     // Send OTP to the user's email
 //     sendOTPByEmail(email, OTP);
-  
+
 //     res.status(200).json({ message: 'OTP sent successfully' });
 //   };
 
 //   exports.verifyOTP = (req, res) => {
 //     const { email, otp } = req.body;
-  
+
 //     // Validate OTP (You need to implement this function)
 //     if (validateOTP(email, otp)) {
 //       // OTP is valid, authenticate user
@@ -181,7 +326,7 @@ exports.logout = async (req, res) => {
 //       },
 //     })
 //   );
-  
+
 //   function sendOTP(email, otp) {
 //     const mailOptions = {
 //       from: 'mohammadsaad0606@gmail.com', // Sender's email address
@@ -189,7 +334,7 @@ exports.logout = async (req, res) => {
 //       subject: 'OTP for Sign In',
 //       text: `Your OTP for sign in is: ${otp}`,
 //     };
-  
+
 //     transporter.sendMail(mailOptions, function (error, info) {
 //       if (error) {
 //         console.log(error);
@@ -235,4 +380,119 @@ exports.logout = async (req, res) => {
 //         });
 //         res.status(200).json({ token });
 //     });
+// };
+
+// const connection = mysql2.createConnection({
+//     host: 'localhost',
+//     port: '3306',
+//     user: 'root',
+//     password: '0987poiu',
+//     database: 'saad'
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to database:', err);
+//         return;
+//     }
+//     console.log('Connected to database');
+// });
+
+// function generateOTP() {
+//     return Math.floor(100000 + Math.random() * 900000);
+// }
+
+// // Send OTP via email
+// async function sendOTP(email, otp) {
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: 'mohammadsaad0606@gmail.com',
+//             pass: 'zuij xxpl rrod ttif'
+//         }
+//     });
+
+//     const mailOptions = {
+//         from: 'mohammadsaad0606@gmail.com',
+//         to: email,
+//         subject: 'OTP for Login',
+//         text: `Your OTP for login is: ${otp}`
+//     };
+
+//     await transporter.sendMail(mailOptions);
+// }
+
+
+
+// function storeOTP(email, otp) {
+//     const query = 'INSERT INTO otps (email, otp) VALUES (?, ?) ON DUPLICATE KEY UPDATE otp = VALUES(otp)';
+//     connection.query(query, [email, otp], (error, results) => {
+//         if (error) {
+//             console.error('Error storing OTP:', error);
+//         } else {
+//             console.log('OTP stored successfully');
+//         }
+//     });
+// }
+
+// const getStoredOTP = async (email)=> {
+//     // const query = 'SELECT otp, timestamp FROM otps WHERE email = ?';
+//     try{
+//     const query = 'SELECT otp, timestamp FROM otps WHERE email = ? ORDER BY timestamp DESC LIMIT 1';
+//     return  connection.query(query, [email])
+
+// }catch(error){
+//     console.log(error)
+// }
+//         // (error, results) => {
+//         // if (error) {
+//         //     console.error('Error retrieving OTP:', error);
+//         //     callback(error, null);
+//         // } else if (results.length === 0) {
+//         //     callback(null, null); // No OTP found for the email
+//         // } else {
+//         //     const storedOTP = results[0];
+//         //     callback(null, storedOTP);
+//         //     // console.log(storedOTP);
+//         // }
+//     // });
+// }
+
+// exports.loginByOtp = async (req, res) => {
+//     try {
+//     const { email } = req.body;
+//     const otp = generateOTP();
+
+//     // Store OTP in database
+//     storeOTP(email, otp);
+
+    
+//         await sendOTP(email, otp);
+//         res.status(200).json({ message: 'OTP sent successfully' })
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// };
+
+// exports.verifyOtp = async (req, res) => {
+//     try {
+//         const { email, otp } = req.body;
+//        const storedOTP = await  getStoredOTP(email);
+//         if (!storedOTP || storedOTP.otp != otp) {
+//             console.log("otp : ",otp)
+//             console.log("newwwwwwwwww",storedOTP)
+//             return res.status(401).json({ error: 'Invalid OTP' })
+
+//         }
+//         // Check OTP expiration (e.g., 5 minutes)
+//         const otpTimestamp = new Date(storedOTP.timestamp).getTime();
+//         if (Date.now() - otpTimestamp > 5 * 60 * 1000) {
+//             return res.status(401).json({ error: 'OTP Expired' })
+//         }
+//         // Successful login
+//         res.status(200).json('Login successful');
+//     } catch (error) {
+//         console.error('Error in verifying OTP', error);
+//         return res.status(500).json({ error: error.message });
+//     }
 // };
--- orders_api_demo/final/routes/auth.js
+++ orders_api_demo/final/routes/auth.js
@@ -4,7 +4,11 @@
 
 const express = require('express');
 const router = express.Router();
+const mysql2 = require('mysql2');
 const auth= require('../controllers/auth');
+
+// const nodemailer = require('nodemailer');
+// const crypto = require('crypto');
 
 const authenticateToken = require('../middleware/authentication');
 const authAdmin = require('../middleware/authAdmin')
@@ -17,8 +21,114 @@ router.post('/logout', auth.logout);
 // router.post('/login-with-otp', auth.loginWithOTP);
 // router.post('/request-otp', auth.requestOTP);
 
-// router.post('/sendOTP', auth.sendOTP);
-// router.post('/verifyOTP', auth.verifyOTP);
+router.post('/sendOTP', auth.sendOTP);
+router.post('/verifyOTP', auth.verifyOTP);
+
+// const connection = mysql2.createConnection({
+//     host: 'localhost',
+//   port: '3306',
+//   user: 'root',
+//   password: '0987poiu',
+//   database: 'saad'
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to database:', err);
+//         return;
+//     }
+//     console.log('Connected to database');
+// });
+
+// function generateOTP() {
+//     return Math.floor(100000 + Math.random() * 900000);
+// }
+
+// // Send OTP via email
+// async function sendOTP(email, otp) {
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: process.env.EMAIL_USER,
+//             pass: '1234##qweasd'
+//         }
+//     });
+
+//     const mailOptions = {
+//         from: 'msaads0606@gmail.com',
+//         to: email,
+//         subject: 'OTP for Login',
+//         text: `Your OTP for login is: ${otp}`
+//     };
+
+//     await transporter.sendMail(mailOptions);
+// }
+
+// // Dummy database to store OTPs
+// function storeOTP(email, otp) {
+//     const query = 'INSERT INTO otps (email, otp) VALUES (?, ?)';
+//     connection.query(query, [email, otp], (error, results) => {
+//         if (error) {
+//             console.error('Error storing OTP:', error);
+//         } else {
+//             console.log('OTP stored successfully');
+//         }
+//     });
+// }
+
+// function getStoredOTP(email, callback) {
+//     const query = 'SELECT otp, timestamp FROM otps WHERE email = ?';
+//     connection.query(query, [email], (error, results) => {
+//         if (error) {
+//             console.error('Error retrieving OTP:', error);
+//             callback(error, null);
+//         } else if (results.length === 0) {
+//             callback(null, null); // No OTP found for the email
+//         } else {
+//             const storedOTP = results[0];
+//             callback(null, storedOTP);
+//         }
+//     });
+// }
+
+// // Endpoint to initiate login with OTP
+// router.post('/loginByOtp', async (req, res) => {
+//     const { email } = req.body;
+//     const otp = generateOTP();
+
+//     // Store OTP in database
+//     storeOTP(email, otp);
+
+//     try {
+//         await sendOTP(email, otp);
+//         res.send('OTP sent successfully');
+//     } catch (error) {
+//         res.status(500).json({error:error.message});
+//     }
+// });
+
+// // Endpoint to verify OTP and login
+// router.post('/verifyOtp', (req, res) => {
+//     const { email, otp } = req.body;
+//     getStoredOTP(email, (error, storedOTP) => {
+//         if (error) {
+//             return res.status(500).json({error: error.message});
+//         }
+
+//         if (!storedOTP || storedOTP.otp !== otp) {
+//             return res.status(401).send('Invalid OTP');
+//         }
+
+//         // Check OTP expiration (e.g., 5 minutes)
+//         const otpTimestamp = new Date(storedOTP.timestamp).getTime();
+//         if (Date.now() - otpTimestamp > 5 * 60 * 1000) {
+//             return res.status(401).send('OTP expired');
+//         }
+
+//         // Successful login
+//         res.send('Login successful');
+//     });
+// });
 
 module.exports = router;
 
